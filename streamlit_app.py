fe3d810cec5d9cdc02c8d34c3e981ee61a5f11e7
SureshKH <hsuresh@hotmail.com>
2024-12-29 16:53:32 +0530

    Update streamlit_app.py
    Translating Fruit Name to Search_On field

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options")\
 #st.stop
 # Convert the Snowpark dataframe to pandas dataframe so that we can use iloc feature
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 ingredients_list = st.multiselect('Choose upto 5 ingredients:'
                                   , my_dataframe
                                   , max_selections=5)
@@ -30,6 +30,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrient Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
